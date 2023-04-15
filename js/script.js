e02fb913784ff9ce7154f81ebf502093bf4ad5d7
r2d23e <kildiyarov.timur13@gmail.com>
2023-04-15 09:43:28 +0500

    main

--- js/script.js
+++ js/script.js
@@ -1,39 +1,68 @@
-let doc = document
+// let doc = document//получение объекта body
 
-let body = doc.body
-let html = doc.documentElement
-
-let head = body.previousElementSibling
-let h_cal = body.children
-
-console.log(body.childNodes);
-console.log(body.children);
-console.log(body);
-console.log(head);
-console.log(html);
+// let body = doc.body//обращение к body
+// let html = doc.documentElement//обращение к самому домену
 
 
-let inp = body.firstElementChild
+// let head = body.previousElementSibling
+// let h_cal = body.children
 
-let div = body.lastElementChild.previousElementSibling//следующий элемент//последний
-let div2 = body.firstElementChild.nextElementSibling//предыдущий элемент
-let inp2 = body.children[0]
+// console.log(head);
+// console.log(h_cal);
 
-console.log(inp, inp2, div,);
+// console.log(body);
+// console.log(html);
 
-console.log(div);
+// console.log(body.childNodes);
+// console.log(body.children);
 
-let deti = div.children[1]
-let roditel = deti.parentNode
+// let inp = body.firstElementChild
+// let inp2 = body.children[0]
+// let div = body.lastElementChild.previousElementSibling//следующий элемент//последний
+// let div2 = body.firstElementChild.nextElementSibling//предыдущий элемент
 
-console.log(deti, roditel);
 
-deti.style.color = "red"
-deti.style.backgroundColor = "blue"
-deti.style.fontsize = "30px"
-deti.style.width = "100px"
+// console.log(inp, inp2, div,);
 
-let div1 = doc.querySelector('.wrap1').nextElementSibling
-div1.classList.add('.bl')
-console.log(div1);
-div1.classList.add('.bl')
+// console.log(inp);
+
+// console.log(div);
+
+// let deti = div.children[1]
+// let roditel = deti.parentNode
+
+// console.log(deti, roditel);
+
+// // deti.style.color = "red"
+// // deti.style.backgroundColor = "blue"
+// // deti.style.fontsize = "30px"
+// // deti.style.width = "100px"
+
+// let div1= doc.querySelector('.wrap1').firstElementChild
+
+// console.log(div1);
+// div1.classList.add('bl')
+
+
+
+let doc = document//получение объекта body
+
+let body = doc.body//обращение к body
+let html = doc.documentElement//обращение к самому домену
+
+let div = doc.querySelector('div')
+div.classList.add('wrap')
+
+let shadow = doc.querySelector('div').lastChild
+
+
+
+let header= doc.querySelector('div').firstElementChild
+header.classList.add('hed')
+
+
+let ul = doc.querySelector('header').lastElementChild
+ul.classList.add('ul')
+console.log(ul);
+let li = doc.querySelector('ul')
+li.classList.add('tw')
